8acd53242154ffff5f43138a905e6e471060eb9b
Chani Kaufman <ckaufman@by.local>
2025-07-20 17:04:32 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,17 @@
-import React, {useState} from "react";
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
 
-    function handleResponse(response) {
-        console.log(response.data);
-       
-    }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function search(event) {
     event.preventDefault();
-
+    //documenation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "2d0a8749coc87c3f1a0at1425f4adb36";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
